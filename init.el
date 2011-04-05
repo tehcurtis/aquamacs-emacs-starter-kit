5b82cc269af5501f4ebe740391d6955ee88718cd
Curtis Edmond <curtis.edmond@gmail.com>
2011-04-05 06:29:19 -0600

    removing unnecessary load

--- init.el
+++ init.el
@@ -39,7 +39,6 @@
 
 ;;;;; Vendor libs
 
-(add-to-list 'load-path "/path/to/full-ack")
 (autoload 'ack-same "full-ack" nil t)
 (autoload 'ack "full-ack" nil t)
 (autoload 'ack-find-same-file "full-ack" nil t)
