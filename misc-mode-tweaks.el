2da61dd1ddf5febc0db318b1878da9f63c0a5a1b
Curtis Edmond <curtis.edmond@gmail.com>
2011-07-09 01:39:49 -0600

    remove ruby-electric and ri

--- misc-mode-tweaks.el
+++ misc-mode-tweaks.el
@@ -18,18 +18,18 @@
 ;; most of the time I like ruby-electric, though sometimes it can be a pain
 ;; also adding flymake for syntax checking (also sometimes can be a pain)
 ;; if either is a pain, toggle them off momentarily
-(require 'ruby-electric)
-(add-hook 'ruby-mode-hook
-          (lambda nil
-            (require 'ruby-electric)
-            (ruby-electric-mode)
-            (flymake-mode-on)))
+;(require 'ruby-electric)
+;(add-hook 'ruby-mode-hook
+;          (lambda nil
+;            (require 'ruby-electric)
+;            (ruby-electric-mode)
+;            (flymake-mode-on)))
 
 ;; emacs wrapper for ri (replaces old ri.el)
 ;; see http://www.emacswiki.org/cgi-bin/wiki/RiEl for details
 ;; however, the latest code was grabbed from
 ;; http://github.com/technomancy/emacs-starter-kit/(github commit gobbly gook)/elpa-to-submit/ri.el
 ;; may slow Aquamacs startup
-(require 'ri)
+;(require 'ri)
 
 (provide 'misc-mode-tweaks)
